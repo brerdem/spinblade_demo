db8368a677bb1da1bffe3457e1ac5efcaadb36c5
Burak Erdem <falsemech@gmail.com>
2022-04-18 11:21:42 +0300

    dat.gui added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 import {LoremIpsum} from "lorem-ipsum";
 
 import * as THREE from "three";
+import * as dat from 'dat.gui';
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 import {RGBELoader} from "three/examples/jsm/loaders/RGBELoader";
-import {MathUtil} from "three/examples/jsm/libs/OimoPhysics";
 import {TextureLoader} from "three";
+
+
+const settings = {
+  textVisible: true,
+  gridVisible: true,
+}
+
+
 
 const lorem = new LoremIpsum({
   sentencesPerParagraph: {
@@ -19,15 +27,23 @@ const lorem = new LoremIpsum({
   suffix: "\n\n\n"
 });
 
-const elem = document.getElementById("site");
-elem.innerText = lorem.generateParagraphs(50);
+let renderer, camera, mixer, clipDuration, totalScrollHeight, object, subObject, elem;
 
-let renderer, camera, mixer, clipDuration, totalScrollHeight;
+const gui = new dat.GUI({name: 'Controls', width: 350});
+
+elem = document.getElementById("site");
+
+function changeVisibility() {
+  elem.style.visibility = settings.textVisible ? 'visible' : 'hidden'
+}
+
+
+elem.innerText = lorem.generateParagraphs(50);
 //let controls;
 
 let scene = new THREE.Scene();
 camera = new THREE.PerspectiveCamera(
-  54,
+  50,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
@@ -44,28 +60,50 @@ renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 1;
 // document.body.appendChild(renderer.domElement);
 
-camera.position.x = 1.5;
-camera.position.y = 1.5;
+camera.position.x = 2;
+camera.position.y = 0;
 camera.position.z = 0;
-camera.lookAt(0, 0.5, 0);
+camera.lookAt(0, 0, 0);
 
-const light = new THREE.AmbientLight( 0xFFFFFF, 2 ); // soft white light
-scene.add( light );
+const clock = new THREE.Clock();
 
 /*
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0);
-scene.add(directionalLight);
-
-const helper = new THREE.DirectionalLightHelper( directionalLight, 5, 'red' );
-scene.add( helper );
+const axesHelper = new THREE.AxesHelper( 10 );
+scene.add( axesHelper );
 */
 
-const spotLight = new THREE.SpotLight(0xffffff, 10, 25, -Math.PI / 6);
-spotLight.position.set(-2, 2, 0);
+const gridHelper = new THREE.GridHelper( 10, 10 );
+scene.add( gridHelper );
+
+/*const light = new THREE.AmbientLight( 0xFFFFFF, 1 ); // soft white light
+scene.add( light );*/
+
+const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 10 );
+scene.add( light );
+
+
+const dirLight1 = new THREE.DirectionalLight( 0xffffff, 0.6);
+dirLight1.position.set( 1, 0, 0 );
+scene.add( dirLight1 );
+
+gui.add(settings, 'textVisible').name(`Text`).onChange(() => changeVisibility());
+gui.add(settings, 'gridVisible').name(`Grid`).onChange(() => {
+  gridHelper.visible = settings.gridVisible;
+});
+gui.add(dirLight1, 'intensity', 0, 1).name(`Intensity Dir Light`);
+
+
+
+
+const spotLight = new THREE.SpotLight(0xffffff, 6, 25, -Math.PI / 6);
+spotLight.position.set(-1, 3, 0);
 spotLight.castShadow = true;
 spotLight.shadow.mapSize.width = 1024;
 spotLight.shadow.mapSize.height = 1024;
 scene.add(spotLight);
+
+gui.add(spotLight, 'intensity', 0, 10).name(`Spot Int`);
+gui.add(spotLight.position, 'y', 2, 5).name(`Spot Y`);
 
 /*const spotHelper = new THREE.SpotLightHelper(spotLight, 'red');
 scene.add(spotHelper);*/
@@ -92,20 +130,25 @@ rgbeLoader.load('https://threejs.org/examples/textures/2294472375_24a3b8ef46_o.j
 
       const scale = 0.3
 
-      let object = gltf.scene;
+      object = gltf.scene;
 
       object.scale.set(scale, scale, scale);
+      object.rotation.setFromVector3(new THREE.Vector3(0, Math.PI/2, 0))
+
       object.castShadow = true;
       object.receiveShadow = true;
 
       object.traverse((o) => {
         if (o.isMesh) {
 
-          o.material.reflectivity = 1;
+          if (o.name === 'Cube002_1') subObject = o;
+          gui.add(o.material, 'metalness', 0, 1).name(`Metalness ${o.name}`);
+
+       /*   o.material.reflectivity = 1;
           o.material.metalness = 1;
-          o.material.needsUpdate = true;
           o.material.envMapIntensity = 0.8;
           o.material.roughness = 0.15
+          o.material.needsUpdate = true;*/
         }
       });
 
@@ -117,7 +160,7 @@ rgbeLoader.load('https://threejs.org/examples/textures/2294472375_24a3b8ef46_o.j
 
         const animAction = mixer.clipAction(clip);
         //animAction.loop = THREE.LoopRepeat;
-        animAction.loop = THREE.LoopPingPong;
+        animAction.loop = THREE.LoopRepeat;
         //animAction.timeScale = 1;
 
         animAction.play();
@@ -125,7 +168,7 @@ rgbeLoader.load('https://threejs.org/examples/textures/2294472375_24a3b8ef46_o.j
       });
 
       scene.add(object);
-      directionalLight.target = object
+
     },
     function (xhr) {
       console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
@@ -139,39 +182,81 @@ rgbeLoader.load('https://threejs.org/examples/textures/2294472375_24a3b8ef46_o.j
 
 });
 
+let direction = 1;
+
+
 let animate = function () {
   requestAnimationFrame(animate);
+
+
+
+
+  if (object && mixer) {
+    if (mixer.time > 0.8) {
+
+      object.rotation.y += clock.getDelta();
+    } else {
+      object.rotation.y = THREE.MathUtils.degToRad(90)
+    }
+
+
+  }
+
+
   renderer.render(scene, camera);
+
+  //const a = object.getObjectByName('Cylinder005');
+
+
 };
 
 animate();
 
 let oldValue = 0;
 
+
 window.addEventListener('load', function (e) {
+
+
   totalScrollHeight = window.document.documentElement.scrollHeight - window.innerHeight;
 });
 
 window.addEventListener('scroll', function (e) {
 
 
-  console.log('totalScrollHeight -->', totalScrollHeight);
+/*
+  if (camera.position.y <= 0) {
+    direction = 1;
+  } else if (camera.position.y >= 2) {
+    direction = -1
+  }*/
 
-  let newValue = window.pageYOffset;
-  //console.log('newValue -->', newValue);
-  /*
-    const diff = (newValue / totalScrollHeight) * 3;
+const offset = window.pageYOffset;
+
+
+  camera.position.y = THREE.MathUtils.mapLinear(offset, 0, totalScrollHeight, 0, 2);
+  camera.lookAt(0, camera.position.y * 0.5, 0);
+  camera.updateProjectionMatrix();
+
+
+  subObject.rotateX(offset * 0.01);
+
+
+
+  //let newValue = window.pageYOffset;
+
+/*   const diff = (newValue / totalScrollHeight) * 3;
     console.log('diff -->', diff);
 
 
     camera.position.y = -0.5 + diff;
     camera.lookAt(0, 0.5 - (diff * 0.1), 0)*/
 
-  mixer.update((newValue - oldValue) / 1000);
-  oldValue = newValue;
+  //mixer.update((newValue - oldValue) / 1000);
+  //oldValue = newValue;
  /* const time = (window.pageYOffset / totalScrollHeight) * clipDuration;
-  console.log('time -->', time);
-  mixer.setTime(MathUtil.clamp(time, 0, clipDuration));*/
+  console.log('time -->', time);*/
+  mixer.setTime(THREE.MathUtils.mapLinear(offset, 0, totalScrollHeight, 0, clipDuration-0.0001));
 });
 
 window.addEventListener('resize', onWindowResize, false);
